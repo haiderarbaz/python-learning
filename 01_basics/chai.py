0e740c7dc902e9e4002427a9bfbee3cb2b56a301
haiderarbaz <admin@arbazhaider.com>
2025-06-15 16:55:48 +0000

    Add 01_basics/ folder to codebase

---
+++ 01_basics/chai.py
@@ -0,0 +1,4 @@
+from hello_chai import chai
+from hello_chai import userName
+chai("ginger tea")
+userName("Arboo")
---
+++ 01_basics/hello_chai.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+
+def chai(n): #def is definition
+    print(n)
+
+chai(4)
+chai("Lemon Tea")
+chai("Arbaz Haider")
+
+def userName(n):
+    print(n)
+
+userName("Arbaz Haider")
